ff5bad45d40035b58117a1f30fd585e998c42431
Showta Ishizaki <zakinko@snowrabbit.org>
2026-09-24 12:40:47 +0900

    ci: HAVE_SHLIB を足す実験は前提が誤りだったので差し替える
    xemacs.mak の OPT_DEFINES に -DHAVE_SHLIB が無いのを見て「だから無効」と
    読んだが、探す場所が違っていた。Windows は configure を走らせず、
    src/config.h.in をそのまま config.h にコピーして使い (nt/xemacs.mak 937)、
    その config.h が後から s/windowsnt.h を読む (config.h.in 903)。
    s/windowsnt.h は #define HAVE_SHLIB しているので (86)、312 行の #undef は
    上書きされる。emodules.obj と sysdll.obj も object 一覧に入っていて、
    emacs.c は #ifdef HAVE_SHLIB で vars_of_module() を呼び、そこが
    (provide (quote modules)) する。経路は繋がっている。
    
    足す実験には意味が無いので、object が入っていることと定義があることを
    見せる段に変える。probe の方は load-module を実際に呼んで、機能が無いのか
    対象が無いのかを返る error で見分けられるようにした。

--- ci/xemacs/win-modules-probe.el
+++ ci/xemacs/win-modules-probe.el
@@ -1,14 +1,25 @@
 ;; Windows で建てた XEmacs に動的ロード(モジュール)が入っているかを見る。
 ;;
-;; src/sysdll.c には WIN32_NATIVE 用の LoadLibrary/GetProcAddress 実装が
-;; 既に在るが、その全体が #ifdef HAVE_SHLIB の中で、nt/xemacs.mak の
-;; OPT_DEFINES に -DHAVE_SHLIB が無い。emodules.c は (provide 'modules) を
-;; するので、Lisp から見えるかどうかがそのまま答になる。
+;; 当初「xemacs.mak の OPT_DEFINES に -DHAVE_SHLIB が無いから無効」と読んだが、
+;; それは探す場所が違っていた。Windows は configure を走らせず、
+;; src/config.h.in をそのまま config.h にコピーして使い (nt/xemacs.mak 937)、
+;; その中で後から s/windowsnt.h を読む (config.h.in 903)。s/windowsnt.h は
+;; #define HAVE_SHLIB しているので (86)、312 行の #undef はそれに上書きされる。
+;; emodules.obj と sysdll.obj も object 一覧に入っている。
 ;;
-;; -eval を shell から渡すと引用が入れ子になって壊れる。file に置いて
-;; -load する。
-(princ (format "modules=%s load-module=%s unload-module=%s\n"
-               (featurep 'modules)
-               (fboundp 'load-module)
-               (fboundp 'unload-module)))
+;; つまり経路は繋がっていて、modules は t で返るはず。「Windows のモジュール
+;; 対応はまだ」が何を指すのかは、ここから先を測らないと分からない。
+(princ (format "modules=%s\n" (featurep 'modules)))
+(princ (format "load-module=%s unload-module=%s\n"
+               (fboundp 'load-module) (fboundp 'unload-module)))
 (princ (format "configuration=%s\n" system-configuration))
+;; 実際に load-module を呼んで、どこで止まるかを見る。存在しない名前でよい。
+;; 「機能が無い」のか「使えるが対象が無い」のかは、返る error で分かれる。
+(princ (format "module-load-path=%s\n"
+               (if (boundp 'module-load-path) module-load-path "(unbound)")))
+(princ (format "module-extensions=%s\n"
+               (if (boundp 'module-extensions) module-extensions "(unbound)")))
+(condition-case err
+    (progn (load-module "no-such-module-here")
+           (princ "load-module: 例外なしで返った\n"))
+  (error (princ (format "load-module: %S\n" err))))
